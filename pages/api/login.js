bd0200833da71069ae84509caf1e400bade1aa48
Pablo-A-Vargas-Islas <100396937+Pablo-A-Vargas-Islas@users.noreply.github.com>
2024-04-19 11:41:28 -0600

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,26 +1,6 @@
 //api/loging.js
 
-import { Pool } from "pg";
-
-// Declarar conn sin inicializar
-let conn;
-
-// Función para inicializar la conexión
-async function initPool() {
-  conn = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "rds-ejemplo.cle0q44yg71c.us-east-2.rds.amazonaws.com",
-    port: "5432",
-    database: "postgres",
-  });
-
-  // Esperar a que la conexión se establezca
-  await conn.connect();
-}
-
-// Inicializar la conexión
-initPool();
+import conn from "./connection.js";
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
@@ -42,5 +22,6 @@ export default async function handler(req, res) {
     console.error('Error al verificar las credenciales:', error.message);
     return res.status(500).json({ error: 'Internal Server Error', message: 'Error al verificar las credenciales' });
   }
-  }
+}
+
   
